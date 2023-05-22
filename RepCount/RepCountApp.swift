a4df37b00568abe20f635517f63390373f30591a
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-05-22 23:52:01 +0200

    Initial Commit

---
+++ RepCount/RepCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepCountApp.swift
+//  RepCount
+//
+//  Created by Hossein Sharifi on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RepCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
